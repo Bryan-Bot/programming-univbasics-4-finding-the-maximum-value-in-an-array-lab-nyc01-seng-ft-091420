95b6d71597f6f717a170a7ff6b5ec7b8a7bebd3b
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-19 15:51:34 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,11 @@
-def find_max_value (Array)
-Count = 0
-Max = 0
-While count < Array.length do #while loop to iterate over the array
-If Max < Array[count] #If statement that checks if max is less than the array element
-		Max = Array[count] #If true Max is reassigned to the greater number in the array
+def find_max_value (array)
+count = 0
+max = 0
+while count < array.length do #while loop to iterate over the array
+if max < array[count] #If statement that checks if max is less than the array element
+		max = array[count] #If true Max is reassigned to the greater number in the array
 	end
-Count += 1
-End
-   Max
+count += 1
+end
+   max
 end
